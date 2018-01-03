51c40f039ed963783b1e0c5494101da79d8e752f
koooge <koooooge@gmail.com>
2018-01-03 13:40:05 +0000

    response json

--- server.go
+++ server.go
@@ -7,17 +7,21 @@ import (
 )
 
 func foo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Foo")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"name", "foo"}`))
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bar")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"name", "bar"}`))
 }
 
 func main() {
 	l, _ := net.Listen("tcp", ":8080")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/foo", foo)
-	mux.HandleFunc("/bar", bar)
+	mux.HandleFunc("/foo/", foo)
+	mux.HandleFunc("/bar/", bar)
 	fmt.Println(http.Serve(l, mux))
 }
